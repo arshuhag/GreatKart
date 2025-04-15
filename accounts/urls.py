80f8818160d235999528b568cea8a3ec8fc7bbd2
Md. Anisur Rahaman Shuhag <arshuhag.cse@gmail.com>
2025-04-15 16:16:54 +0600

    login and reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('', views.dashboard, name="dashboard"),
     
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgotPassword/', views.forgotPassword, name="forgotPassword"),
+    path('resetPasswordValidate/<uidb64>/<token>/', views.resetPasswordValidate, name="resetPasswordValidate"),
+    path('resetPassword/', views.resetPassword, name="resetPassword"),
 ]
