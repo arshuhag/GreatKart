2e516d6fe389e983765c4b16c36eeba0c869838d
arshuhag <arshuhag.cse@gmail.com>
2025-04-22 21:49:34 +0600

    Update ALLOWED_HOSTS for production

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['greatkart-wiae.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
